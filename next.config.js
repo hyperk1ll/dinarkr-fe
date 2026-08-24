0e8791a02105737da760b69ad05c8613a472d4d1
Hilmy Tsany <hilmy.tsani@gmail.com>
2026-08-24 23:48:57 +0700

    fix cors error

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
   images: {
     domains: ['drive.google.com', 'dinarkr.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api-proxy/:path*',
+        // Rewrite to the backend URL, defaulting to the private IP.
+        destination: `${process.env.BACKEND_URL || 'http://10.50.0.1:7000/api'}/:path*`
+      }
+    ];
+  },
 };
